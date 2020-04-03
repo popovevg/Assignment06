c72bf9c3a16e5b42259cf16729596bce48b97919
popovevg <popovevg2013@gmail.com>
2020-04-02 20:57:55 -0700

    02 April

---
+++ js/dom-practice.js
@@ -0,0 +1,86 @@
+/*eslint-env browser*/
+
+//STEP 1
+//See HTML file
+
+//STEP 2
+// function myFunction() {
+// document.getElementById("Step2button").onclick = alert("I have been clicked");
+// };
+
+//STEP 3
+// window.addEventListener("load", function () {
+//     var button = document.getElementById("Step3button");
+//     button.addEventListener("click", popup);
+//     function popup () {
+//         alert("I have been clicked");
+//     };
+// });
+
+//STEP 4
+// window.addEventListener("load", function () {
+//     let button = document.getElementById("Step4button");
+//     button.addEventListener("click", function() {
+//         alert("I have been clicked");
+//     });
+// });
+
+//STEP 5
+// window.addEventListener("load", function () {
+//     let button = document.getElementById("Step5button");
+//     button.addEventListener("click", function() {
+//         alert("I have been clicked");
+//     });
+// });
+
+//STEP 6
+// window.addEventListener("load", function() {
+//     let link = document.getElementById("redirect").addEventListener("click", function(event) {
+//         event.preventDefault();
+//         window.alert("You clicked link.");
+
+//     })
+// });
+
+//STEP 7
+// window.addEventListener("load", function() {
+//     let box = document.getElementById("userInput");
+//     let button = document.getElementById("Step7button");
+//     button.addEventListener("click", function(event) {
+//         button.disabled = true;
+//         alert(box.value);
+//     })
+// });
+
+//STEP 8
+// window.addEventListener("load", function() {
+//     let button = document.getElementById("Step8button");
+//     button.addEventListener("click", function() {
+//         window.open('newpage.html', 'NEW PAGE', 'width=300,height=300');
+//     })
+// });
+
+//STEP 9
+// window.addEventListener("load", function() {
+//     let btnOne = document.getElementById("firstButton");
+//     let btnTwo = document.getElementById("secondButton");
+//     let id;
+//     function message() {
+//         window.console.log("Learning JavaScript is fun!");
+//     }
+//     btnOne.addEventListener("click", function() {
+//         id = window.setInterval(message, 2000);
+//     })
+//     btnTwo.addEventListener("click", function() {
+//         window.clearInterval(id);
+//     })
+// })
+
+//STEP 10
+// window.addEventListener("load", function() {
+//     let znachenie = document.getElementById("myValue");
+//     let knopka = document.getElementById("Step10buttom");
+//     knopka.addEventListener("click", function() {
+//         window.alert("You selected " + znachenie.value);
+//     });
+// })
